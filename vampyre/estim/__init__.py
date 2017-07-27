5dab6b7274f2f16bb78f8e7c9a4a361cc8424469
Sundeep Rangan <srangan@nyu.edu>
2017-07-27 15:56:02 -0400

    Removed dependence on msg_admm.py until that is completed.

--- vampyre/estim/__init__.py
+++ vampyre/estim/__init__.py
@@ -5,7 +5,6 @@ from vampyre.estim.mixture import MixEst
 from vampyre.estim.linear import LinEstim
 from vampyre.estim.linear_two import LinEstimTwo
 from vampyre.estim.msg import MsgHdl, MsgHdlSimp, ListMsgHdl
-from vampyre.estim.msg_robust import MsgHdlAdmm
 from vampyre.estim.discrete import DiscreteEst
 from vampyre.estim.interval import HardThreshEst
 from vampyre.estim.stack import StackEstim
