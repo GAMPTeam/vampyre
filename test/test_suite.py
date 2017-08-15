5637c957d8b71ce8460c0435e6cc1ee5cf6a68a0
Mark Borgerding <mark@borgerding.net>
2017-08-15 13:02:44 -0400

    relu_test seems to have been moved into test/test_estim/

--- test/test_suite.py
+++ test/test_suite.py
@@ -38,7 +38,7 @@ def test_version():
        #['estim.linear_two.lin_two_test_mult',vp.estim.linear_two.lin_two_test_mult],\
        ['estim.interval.gauss_integral_test',vp.estim.interval.gauss_integral_test],\
        ['estim.interval.hard_thresh_test',vp.estim.interval.hard_thresh_test],\
-       ['estim.relu.relu_test',vp.estim.relu.relu_test],\
+       #['estim.relu.relu_test',vp.estim.relu.relu_test],\
        #['solver.vamp.vamp_test_mult',vp.solver.vamp.vamp_test_mult],\
        #['solver.mlvamp.mlvamp_probit_test',vp.solver.mlvamp.mlvamp_probit_test],\
        #['vamp_sparse_test.sparse_inv',vamp_sparse_test.sparse_inv]\
