bdd7ca674dbaff8d7033e0f848512b4fb7dc158e
Sundeep Rangan <srangan@nyu.edu>
2018-03-30 11:12:59 -0400

    Added 2D convolution transform. -- Right now only supports circular convolution.

--- vampyre/trans/__init__.py
+++ vampyre/trans/__init__.py
@@ -3,5 +3,6 @@ from vampyre.trans.base import BaseLinTrans
 from vampyre.trans.matrix import MatrixLT
 from vampyre.trans.tflintrans import TFLinTrans
 from vampyre.trans.wavelet import Wavelet2DLT
+from vampyre.trans.convolve2d import Convolve2DLT
 from vampyre.trans.randmat import rand_rot_invariant_mat
 
